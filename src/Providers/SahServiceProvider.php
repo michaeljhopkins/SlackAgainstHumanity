8b230adaecf1d8371256c721fe806cfb051acf35
Michael Hopkins <michael@appitventures.com>
2015-02-02 22:19:38 -0700

    a little bit of diedumpdrivendevelopment

--- src/Providers/SahServiceProvider.php
+++ src/Providers/SahServiceProvider.php
@@ -10,10 +10,22 @@ class SahServiceProvider extends ServiceProvider
     public function boot()
     {
         include __DIR__ . '/../Http/routes.php';
+
+        $createCardsTable = 'create_cards_table.php';
+        $createPlayersTable = 'create_players_table.php';
+        $this->publishes([
+            __DIR__ . '/../Database/migrations/'. $createCardsTable => $this->app['path.database'].'/database/'. $this->getDatePrefix().$createCardsTable,
+            __DIR__ . '/../Database/migrations/'. $createPlayersTable => $this->app['path.database'].'/database/'.$this->getDatePrefix().$createPlayersTable,
+            __DIR__.'/../Database/seeds' => $this->app['path.database'].'/seeds'
+        ]);
     }
 
     public function register()
     {
 
+    }
+    protected function getDatePrefix()
+    {
+        return date('Y_m_d_His');
     }
 }
