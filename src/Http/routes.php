b6ba410f84fe7450e8c73d246474a4da279afe17
Michael Hopkins <michael@appitventures.com>
2015-02-06 15:16:08 -0700

    calling classes directly, no push queues

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,10 +1,10 @@
 <?php
 
 Route::group(['prefix' => 'cards'], function () {
-    Route::get('deal', function () {Queue::push('\Hopkins\SlackAgainstHumanity\Game\Handler@deal', Input::all());});
-    Route::get('start', function () {Queue::push('\Hopkins\SlackAgainstHumanity\Game\Handler@start', Input::all());});
-    Route::get('show', function () {Queue::push('\Hopkins\SlackAgainstHumanity\Game\Handler@show', Input::all());});
-    Route::post('play', function () {Queue::push('\Hopkins\SlackAgainstHumanity\Game\Handler@play', Input::all());});
-    Route::post('choose', function () {Queue::push('\Hopkins\SlackAgainstHumanity\Game\Handler@choose', Input::all());});
-    Route::get('quit', function () {Queue::push('\Hopkins\SlackAgainstHumanity\Game\Handler@quit', Input::all());});
+    Route::get('deal', '\Hopkins\SlackAgainstHumanity\Game\Handler@deal');
+    Route::get('start', '\Hopkins\SlackAgainstHumanity\Game\Handler@start');
+    Route::get('show', '\Hopkins\SlackAgainstHumanity\Game\Handler@show');
+    Route::post('play', '\Hopkins\SlackAgainstHumanity\Game\Handler@play');
+    Route::post('choose', '\Hopkins\SlackAgainstHumanity\Game\Handler@choose');
+    Route::get('quit', '\Hopkins\SlackAgainstHumanity\Game\Handler@quit');
 });
