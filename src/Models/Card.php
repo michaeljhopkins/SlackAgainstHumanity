e9192d2209e1a1b7d020d4ce5eb7e9d31dfc27bf
Michael Hopkins <michael@appitventures.com>
2015-02-14 21:55:09 -0700

    adding phpdoc blocks

--- src/Models/Card.php
+++ src/Models/Card.php
@@ -1,6 +1,7 @@
 <?php namespace Hopkins\SlackAgainstHumanity\Models;
 
 use Hopkins\GamesBase\Models\Player;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use DB;
 
@@ -34,7 +35,6 @@ use DB;
 class Card extends Model
 {
     protected $guarded = ['id'];
-    protected $fillable = ['dealt','player_id','text','color','in_play'];
     protected $table = 'cards';
 
     public function player()
